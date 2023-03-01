44888c93bc5c7bdb2c1a44f1e1c88990d5e23751
mj-pg <vartxn@gmail.com>
2023-03-01 22:19:01 +0800

    add server

--- main.go
+++ main.go
@@ -1,67 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"strings"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("vim-go")
-}
 
-// Translate turns each word in the text to its pig latin version.
-func Translate(text string) string {
-	ww := strings.Fields(text)
-	res := make([]string, len(ww))
-	for i, word := range ww {
-		pl := pigLatinize(word)
-		res[i] = pl
-	}
-	return strings.Join(res, " ")
-}
-
-// pigLatinize returns the pig latin version of a word.
-func pigLatinize(word string) string {
-	// word is empty
-	if word == "" {
-		return ""
-	}
-
-	// word starts with a vowel, just add suffix
-	if isVowel(word[0]) {
-		return withSuffix(word)
-	}
-
-	// word starts with consonant
-	// move starting consonants to the end then add suffix
+	// server port
 	//
-	start, remaining := splitStart(word)
-	return withSuffix(remaining + start)
-}
+	var port int
+	flag.IntVar(&port, "port", 3000, "port for this service")
 
-// isVowel checks if a character is a vowel.
-func isVowel(letter byte) bool {
-	switch letter {
-	case 'a', 'e', 'i', 'o', 'u',
-		'A', 'E', 'I', 'O', 'U':
-		return true
+	handler := &PigLatinHandler{}
+	http.Handle("/piglatins", handler)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Printf("Server exited with error: %v", err)
 	}
-	return false
 }
 
-// withSuffix appends the suffix 'ay' to the word.
-func withSuffix(word string) string {
-	return word + "ay"
-}
+type PigLatinHandler struct{}
 
-// splitStart splits the starting consonant or cluster from the rest of the word.
-func splitStart(word string) (string, string) {
-	var consonants string
-	for i, letter := range word {
-		if isVowel(byte(letter)) {
-			return consonants, word[i:]
-		}
-		consonants = consonants + string(letter)
+// ServeHTTP routes the request based on the HTTP method.
+func (h *PigLatinHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.Translate(w, r)
+		w.WriteHeader(200)
+		return
+	case http.MethodGet:
+		h.List(w, r)
+		w.WriteHeader(200)
+		return
 	}
-	return consonants, ""
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (h *PigLatinHandler) Translate(w http.ResponseWriter, r *http.Request) {
+}
+
+func (h *PigLatinHandler) List(w http.ResponseWriter, r *http.Request) {
 }
---
+++ piglatin.go
@@ -0,0 +1,60 @@
+package main
+
+import "strings"
+
+// Translate turns each word in the text to its pig latin version.
+func Translate(text string) string {
+	ww := strings.Fields(text)
+	res := make([]string, len(ww))
+	for i, word := range ww {
+		pl := pigLatinize(word)
+		res[i] = pl
+	}
+	return strings.Join(res, " ")
+}
+
+// pigLatinize returns the pig latin version of a word.
+func pigLatinize(word string) string {
+	// word is empty
+	if word == "" {
+		return ""
+	}
+
+	// word starts with a vowel, just add suffix
+	if isVowel(word[0]) {
+		return withSuffix(word)
+	}
+
+	// word starts with consonant
+	// move starting consonants to the end then add suffix
+	//
+	start, remaining := splitStart(word)
+	return withSuffix(remaining + start)
+}
+
+// isVowel checks if a character is a vowel.
+func isVowel(letter byte) bool {
+	switch letter {
+	case 'a', 'e', 'i', 'o', 'u',
+		'A', 'E', 'I', 'O', 'U':
+		return true
+	}
+	return false
+}
+
+// withSuffix appends the suffix 'ay' to the word.
+func withSuffix(word string) string {
+	return word + "ay"
+}
+
+// splitStart splits the starting consonant or cluster from the rest of the word.
+func splitStart(word string) (string, string) {
+	var consonants string
+	for i, letter := range word {
+		if isVowel(byte(letter)) {
+			return consonants, word[i:]
+		}
+		consonants = consonants + string(letter)
+	}
+	return consonants, ""
+}
